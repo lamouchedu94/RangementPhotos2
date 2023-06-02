809e1603e6ac988f4e2cd1b1bfadfb1628e98f29
lamouchedu94 <lamouchedu54@gmail.com>
2023-06-02 12:32:19 +0200

    Create dir

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/fs"
 	"os"
@@ -11,28 +12,59 @@ import (
 	decode "github.com/lamouchedu94/ExifGO"
 )
 
+var path_error = errors.New("no such file or directory")
+
 func main() {
 	r, _ := decode.Read_img("/home/paul/Pictures/Photos/2023/2023.04/2023.04.30/3H2A6646.CR3")
 	t, _ := decode.Image_date(r, ".CR3")
-	dir := final_dir("/home/paul/", t)
+	_, err := final_dir("/home/paul/Pictures/TESTGO", t)
 	//err := run("")
-	test := check_dir(dir, time.Time{})
-	fmt.Println(test)
-	/*
-		if err != nil {
-			fmt.Println(err)
-		}
-	*/
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 }
 
-func final_dir(dest_path string, date time.Time) string {
+func final_dir(dest_path string, date time.Time) (string, error) {
+	//Creé les dossiers si manquants et revoie le chemin avec date
+
+	if string(dest_path[len(dest_path)-1]) != "/" {
+		dest_path += "/"
+	}
+	if !check_dir(dest_path) {
+		fmt.Print(dest_path + ": ")
+		return "", path_error
+	}
+
 	y := date.Year()
-	dest_path += strconv.Itoa(y)
-	m := int(date.Month())
-	dest_path += "/" + strconv.Itoa(m)
+	dest_path += fmt.Sprintf("%d", y)
+	create_dir(dest_path)
+
+	m_temp := int(date.Month())
+	m := strconv.Itoa(m_temp)
+	if len(m) < 10 {
+		m = "0" + m
+	}
+	dest_path += fmt.Sprintf("/%d.%s", y, m)
+	create_dir(dest_path)
+
 	d := date.Day()
-	dest_path += "/" + strconv.Itoa(d)
-	return dest_path
+	dest_path += fmt.Sprintf("/%d.%v.%d", y, m, d)
+	create_dir(dest_path)
+
+	return dest_path, nil
+}
+
+func create_dir(path string) error {
+	if !check_dir(path) {
+		err := os.Mkdir(path, 0750)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+	}
+	return nil
 }
 
 func run(path string) error {
@@ -67,7 +99,7 @@ func run(path string) error {
 	return err
 }
 
-func check_dir(destination_path string, date time.Time) bool {
+func check_dir(destination_path string) bool {
 	//false, no such file directory
 	_, err := os.Stat(destination_path)
 	return err == nil
