2323e8a203827b40eb0f2981dcf524dd81d83b0d
lamouchedu94 <lamouchedu54@gmail.com>
2023-06-02 08:56:06 +0200

    complete dir

--- main.go
+++ main.go
@@ -3,16 +3,36 @@ package main
 import (
 	"fmt"
 	"io/fs"
+	"os"
 	"path/filepath"
+	"strconv"
+	"time"
 
 	decode "github.com/lamouchedu94/ExifGO"
 )
 
 func main() {
-	err := run("")
-	if err != nil {
-		fmt.Println(err)
-	}
+	r, _ := decode.Read_img("/home/paul/Pictures/Photos/2023/2023.04/2023.04.30/3H2A6646.CR3")
+	t, _ := decode.Image_date(r, ".CR3")
+	dir := final_dir("/home/paul/", t)
+	//err := run("")
+	test := check_dir(dir, time.Time{})
+	fmt.Println(test)
+	/*
+		if err != nil {
+			fmt.Println(err)
+		}
+	*/
+}
+
+func final_dir(dest_path string, date time.Time) string {
+	y := date.Year()
+	dest_path += strconv.Itoa(y)
+	m := int(date.Month())
+	dest_path += "/" + strconv.Itoa(m)
+	d := date.Day()
+	dest_path += "/" + strconv.Itoa(d)
+	return dest_path
 }
 
 func run(path string) error {
@@ -45,4 +65,10 @@ func run(path string) error {
 	})
 	fmt.Println(file_count, ": Photo in directory")
 	return err
+}
+
+func check_dir(destination_path string, date time.Time) bool {
+	//false, no such file directory
+	_, err := os.Stat(destination_path)
+	return err == nil
 }
